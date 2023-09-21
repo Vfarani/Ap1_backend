d12bd3ad7dd164d2902ab81386d5b818470c10e2
Vfarani <126683133+Vfarani@users.noreply.github.com>
2023-09-21 11:03:24 -0300

    Add files via upload

--- projeto.py
+++ projeto.py
@@ -1,30 +1,18 @@
-import pydicom
-
+import os
 from flask import Flask, request, jsonify, send_file
-
+import pydicom
 
 app = Flask(__name__)
 
-
-@app.route('/anonimize', methods=['POST'])
-def anonimize_dicom():
-    # Seu código de anonimização aqui
-    return jsonify({"message": "Imagem DICOM anonimizada com sucesso"})
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-def anonimize_dicom_file(input_path, output_path):
+def anonimize_dicom_file(input_path, output_directory):
     ds = pydicom.dcmread(input_path)
     # Aqui, você pode acessar os metadados do DICOM (por exemplo, ds.PatientName)
     # e modificar ou remover informações sensíveis.
     ds.PatientName = "Paciente Anônimo"
     # Outras ações de anonimização, se necessário...
 
+    output_path = os.path.join(output_directory, os.path.basename(input_path))
     ds.save_as(output_path)
-
-
 
 @app.route('/anonimize', methods=['POST'])
 def anonimize_dicom():
@@ -35,21 +23,21 @@ def anonimize_dicom():
     if file.filename == '':
         return jsonify({"error": "Nome de arquivo vazio"})
 
-    input_path = 'temp_input.dcm'
-    output_path = 'temp_output.dcm'
+    # Configure o caminho para a pasta onde você deseja salvar as imagens anonimizadas
+    output_directory = 'C:/Users/202303183445/Desktop/API_backend'
+
+    input_path = os.path.join(output_directory, file.filename)
+
     file.save(input_path)
 
-    anonimize_dicom_file(input_path, output_path)
+    anonimize_dicom_file(input_path, output_directory)
 
     # Envie o arquivo anonimizado como resposta
-    return send_file(output_path, as_attachment=True)
-
-
-    # Envie o arquivo anonimizado como resposta
-    return send_file(output_path)
+    return send_file(input_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
